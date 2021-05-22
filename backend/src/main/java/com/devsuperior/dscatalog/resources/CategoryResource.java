fed35521aeea4c226fad4a2b22758f82a9a1147f
Eduardo Duques <eduardo_gdsw@outlook.com>
2021-05-22 11:50:24 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -53,7 +53,7 @@ public class CategoryResource {
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> update(@PathVariable Long id) {
+	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
