8c02d04c0c3c3fbbeef103e39b7b536eaab00097
Eduardo Duques <eduardo_gdsw@outlook.com>
2021-05-22 14:11:59 -0300

    Product repository, service, resource, GET ok

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -22,7 +22,7 @@ public class Product implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	
 	@Column(columnDefinition = "TEXT")
